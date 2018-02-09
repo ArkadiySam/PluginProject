a893d008ce99976056e3c1144e340499f1df6612
Arkadiy <arksam@list.ru>
2018-02-09 14:00:27 +0500

    WithClassLoader

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,20 +1,15 @@
-/*
-* При компиляции: в sourcepath указать расположение исходного кода, в classpath - расположение  исходных класс и папку Plugins
-* Пример:
-*   javac -sourcepath C:\Users\1\Desktop -cp C:\Users\1\Desktop;C:\Users\1\Desktop\plugins *.java
-*   java -cp C:\Users\1\Desktop;C:\Users\1\Desktop\plugins Solution
-* */
-
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+
+    static String modulePath;
+    public static void main(String[] args){
+
+         modulePath = args[0]+"\\plugins\\";
         String command;
         Scanner scanner=new Scanner(System.in);
         while (true) {
@@ -27,24 +22,44 @@ public class Solution {
         }
     }
     static void getList() {
-        String s = Paths.get("").toAbsolutePath().toString()+"/plugins";
-        File file = new File(s);
-        if (file.exists())
-            for (File item : file.listFiles())
-                System.out.println(item.getName());
+        File dir = new File(modulePath);
+        String[] modules = dir.list();
+        for (String module: modules)
+            System.out.println(module.split(".class")[0]);
     }
 
-    static void execute(String s) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    static void execute(String s){
         ArrayList<String> argPom=new ArrayList<>(Arrays.asList(s.split("\\s+")));
+
         if(argPom.get(0).equals("execute")&&argPom.size()>=3) {
-            Class mClassObject = Class.forName(argPom.get(1));
+            File dir = new File(modulePath);
+            ArrayList<String>pom=new ArrayList<>(Arrays.asList(dir.list()));
+            if(!pom.contains(argPom.get(1)+".class")){
+                System.out.println("Class not exists");
+                return;
+            }
+
+            ModuleLoader loader = new ModuleLoader(modulePath, ClassLoader.getSystemClassLoader());
+            String moduleName = argPom.get(1);
+            Class clazz = null;
+            try {
+                clazz = loader.loadClass(moduleName);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
             argPom.remove(0);
             argPom.remove(0);
             String[] arg = new String[argPom.size()];
             argPom.toArray(arg);
+
             try {
-                Iplugin iplugin = (Iplugin) mClassObject.newInstance();
-                System.out.println(iplugin.execute(arg));
+                Iplugin execute = null;
+                try {
+                    execute = (Iplugin) clazz.newInstance();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(execute.execute(arg));
             } catch (InstantiationException e) {
                 e.printStackTrace();
             }
